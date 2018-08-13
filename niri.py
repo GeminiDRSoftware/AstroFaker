1819b6e02eb8fcff9fce65f88c1341ba679af8bd
csimpson <csimpson@gemini.edu>
2018-08-13 10:36:51 -1000

    don't check for real-data-valid shape

--- niri.py
+++ niri.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from .astrofaker import AstroFaker, noslice
 from gemini_instruments.niri.adclass import AstroDataNiri
 
@@ -8,13 +6,14 @@ class AstroFakerNiri(AstroFaker, AstroDataNiri):
     def add_extension(self, data=None, shape=(1024,1024), pixel_scale=0.11635,
                       pa=0, flip=False):
         """
-        NIRI-specific method which checks for a valid shape and provides
-        default values for the shape and pixel scale.
+        NIRI-specific method which provides NIRI-like defaults. Note that we
+        don't check for valid NIRI data shapes because we may wish to create
+        smaller fake data to speed up computation.
         """
-        if data is not None:
-            shape = data.shape
-        if shape[0] != shape[1] and shape[0] not in (512, 768, 1024):
-            raise ValueError("Invalid NIRI data shape {}".format(shape))
+        #if data is not None:
+        #    shape = data.shape
+        #if shape[0] != shape[1] and shape[0] not in (512, 768, 1024):
+        #    raise ValueError("Invalid NIRI data shape {}".format(shape))
         super(self.__class__, self).add_extension(data=data, shape=shape,
                                 pixel_scale=pixel_scale, pa=pa, flip=flip)
         self[-1].hdr.update({'LOWROW': 0, 'HIROW': shape[0]-1,
