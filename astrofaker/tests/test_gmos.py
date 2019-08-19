deec5709894d79a8c0c90e5b9ac50d25da0bfce3
bquint <bquint@gemini.edu>
2019-08-19 09:56:48 -0400

    Added GMOS tests with questions regarding primitive assignment handling

--- astrofaker/tests/test_gmos.py
+++ astrofaker/tests/test_gmos.py
@@ -31,11 +31,43 @@ def test_can_add_image_extension():
     # ToDo: The line below raises a TypeError if `pixel_scale` is not provided.
     # We need either to provide a better error message to the programmer or
     # set `pixel_scale` with a default value and raise a warning.
-    ad.add_extension(hdu)
+    ad.add_extension(hdu, pixel_scale=1.0)
 
     assert len(ad) == 1
 
 
+def test_can_update_descriptor_dispersion_axis_of_astrodata():
+
+    hdu = fits.ImageHDU()
+    hdu.data = np.random.random((100, 100))
+
+    ad = astrofaker.create('GMOS-S')
+    ad.add_extension(hdu, pixel_scale=1.0)
+
+    # ToDo: how are descriptor assignments handled?
+    # I suspect they should be something like Python Properties. This would
+    # allow some variable validation with a comprehensive error message.
+    ad.dispersion_axis = [1]
+    assert ad.dispersion_axis() == [1]
+
+
+def test_can_update_descriptor_dispersion_axis_of_astrodata_extensions():
+
+    hdu = fits.ImageHDU()
+    hdu.data = np.random.random((100, 100))
+
+    ad = astrofaker.create('GMOS-S')
+    ad.add_extension(hdu, pixel_scale=1.0)
+    ad.add_extension(hdu, pixel_scale=1.0)
+
+    # ToDo: how are descriptor assignments handled?
+    # I suspect they should be something like Python Properties. This would
+    # allow some variable validation with a comprehensive error message.
+    for ext in ad:
+        ext.dispersion_axis = 1
+
+    for ext in ad:
+        assert ext.dispersion_axis == 1
 
 
 if __name__ == '__main__':
